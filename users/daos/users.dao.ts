c4ecad50b4fb1979a6711839b6e2de7b705d2d32
Andrew Elbaneh <aelbaneh0615@gmail.com>
2024-04-15 07:46:51 -0400

    almost finished with mongo

---
+++ users/daos/users.dao.ts
@@ -0,0 +1,32 @@
+import { CreateUserDto } from '../dto/create.user.dto';
+import { PatchUserDto } from '../dto/patch.user.dto';
+import { PutUserDto } from '../dto/put.user.dto';
+import mongooseService from '../../common/services/mongoose.service';
+
+import shortid from 'shortid';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:in-memory-dao');
+
+class UsersDao {
+    users: Array<CreateUserDto> = [];
+
+    constructor() {
+        log('Created new instance of UsersDao');
+    }
+
+    Schema = mongooseService.getMongoose().Schema;
+
+    userSchema = new this.Schema({
+        _id: String,
+        email: String,
+        password: { type: String, select: false },
+        firstName: String,
+        lastName: String,
+        permissionFlags: Number,
+    }, { id: false });
+
+    User = mongooseService.getMongoose().model('Users', this.userSchema);
+}
+
+export default new UsersDao();
--- users/users.routes.config.ts
+++ users/users.routes.config.ts
@@ -1,4 +1,6 @@
 import { CommonRoutesConfig } from '../common/common.routes.config';
+import UsersController from './controllers/users.controller';
+import UsersMiddleware from './middleware/users.middleware';
 import express from 'express';
 
 export class UsersRoutes extends CommonRoutesConfig {
@@ -6,9 +8,34 @@ export class UsersRoutes extends CommonRoutesConfig {
         super(app, 'UsersRoutes');
     }
 
-    configureRoutes() {
-        // (we'll add the actual route configuration here next)
+    configureRoutes(): express.Application {
+        this.app
+            .route(`/users`)
+            .get(UsersController.listUsers)
+            .post(
+                UsersMiddleware.validateRequiredUserBodyFields,
+                UsersMiddleware.validateSameEmailDoesntExist,
+                UsersController.createUser
+            );
+
+        this.app.param(`userId`, UsersMiddleware.extractUserId);
+        this.app
+            .route(`/users/:userId`)
+            .all(UsersMiddleware.validateUserExists)
+            .get(UsersController.getUserById)
+            .delete(UsersController.removeUser);
+
+        this.app.put(`/users/:userId`, [
+            UsersMiddleware.validateRequiredUserBodyFields,
+            UsersMiddleware.validateSameEmailBelongToSameUser,
+            UsersController.put,
+        ]);
+
+        this.app.patch(`/users/:userId`, [
+            UsersMiddleware.validatePatchEmail,
+            UsersController.patch,
+        ]);
+
         return this.app;
     }
-
 }
